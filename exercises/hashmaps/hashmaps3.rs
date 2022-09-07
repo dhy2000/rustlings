73ab7c9f5f8325401f2c52d556f28c499a6bb466
donghanyuan <dhy_120@163.com>
2022-09-07 12:43:42 +0800

    done hashmaps

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,9 +14,10 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::collections::HashMap;
+use std::collections::hash_map::Entry::*;
 
 // A structure to store team name and its goal details.
 struct Team {
@@ -35,11 +36,31 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
+        // Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        for (team_name, scored, conceded) in [
+            (team_1_name, team_1_score, team_2_score),
+            (team_2_name, team_2_score, team_1_score)
+        ] {
+            match scores.entry(team_name) {
+                Occupied(mut e) => {
+                    let team = e.get_mut();
+                    team.goals_scored += scored;
+                    team.goals_conceded += conceded;
+                },
+                Vacant(e) => {
+                    let name = e.key().to_owned();
+                    e.insert(Team {
+                        name,
+                        goals_scored: scored,
+                        goals_conceded: conceded,
+                    });
+                },
+            }
+        }
     }
     scores
 }
